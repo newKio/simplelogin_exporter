ee8c987387270284af850bfbc9ae7df387c2bf39
Daniel <danieldonchev13@pm.me>
2025-06-05 10:40:22 +0100

    Adding script

--- main.py
+++ main.py
@@ -60,5 +60,5 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 
-Original creator: newKio - https://github.com/newKio/HIBP
+Original creator: newKio - https://github.com/newKio/simplelogin_exporter
 '''
